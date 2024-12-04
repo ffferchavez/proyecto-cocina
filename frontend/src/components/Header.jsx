0adb7da16188bcbcc86968cd83d55ca26aa977f5
ffferchavez <ffferchavez@gmail.com>
2024-12-04 11:41:59 +0100

    updated header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => (
   <header className="bg-gray-100 text-gray-800 py-4">
     <div className="container mx-auto flex justify-between items-center">
-      <h1 className="text-2xl">Proyecto Cocina</h1>
-      <nav>
+      <h1 className="text-2xl">Cocina</h1>
+      <nav className='flex gap-4'>
         <a href="#home" className="px-4">Home</a>
         <a href="#menu" className="px-4">Menu</a>
         <a href="#about" className="px-4">About Us</a>
